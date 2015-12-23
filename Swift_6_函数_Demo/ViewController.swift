302d31f2c4b00111c8915b66d0b1903b5a27cc07
Xavier Zachary <xiaxuanzhou@qq.com>
2015-12-23 09:36:48 +0800

    Initial Commit

---
+++ Swift_6_函数_Demo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swift_6_函数_Demo
+//
+//  Created by Jiayu_Zachary on 15/12/23.
+//  Copyright © 2015年 Zachary. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
